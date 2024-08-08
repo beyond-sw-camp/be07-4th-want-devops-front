ac0709664149beaaa8a9f91c06ba43befb60c16c
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-08-08 21:09:05 +0900

    commit

--- src/views/FirstPage.vue
+++ src/views/FirstPage.vue
@@ -3,8 +3,8 @@
     <v-container class="fill-height d-flex justify-center align-center">
       <v-row>
         <v-col cols="12" md="6" class="content">
-          <h1>기존에 경험하지 못한 새로운 여행 플래너</h1>
-          <p class="content2">고민만 하던 여행 계획을 마이로드를 통해 뚝딱 한 번에 스케쥴링 해보세요.</p>
+          <h1>기존에 경험하지 못한 <br/>새로운 여행 플래너</h1>
+          <p class="content2">고민만 하던 여행 계획을<br/> 원트를 통해 한 번에 스케줄링 해보세요</p>
           <GoogleLogin /> <!-- 구글 로그인 컴포넌트 추가 -->
           <v-btn color="primary" @click="startMyRoad">마이로드 시작하기</v-btn>
         </v-col>
