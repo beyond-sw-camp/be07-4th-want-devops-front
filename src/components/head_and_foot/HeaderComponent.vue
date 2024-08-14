e6df77f2205c1540077e12d90b0ff580f1a2cda8
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-08-14 11:39:23 +0900

    firstPage 수정

--- src/components/head_and_foot/HeaderComponent.vue
+++ src/components/head_and_foot/HeaderComponent.vue
@@ -35,7 +35,6 @@ export default {
 
       window.location.href = googleAuthUrl;
     }
-    }
   }
 };
 </script>
--- src/views/FirstPage.vue
+++ src/views/FirstPage.vue
@@ -1,24 +1,20 @@
 <template>
-  <v-app>
-
-    <v-container class="fill-height d-flex justify-center align-center">
-      <v-row>
-        <v-col cols="12" md="6" class="content">
-          <h1>기존에 경험하지 못한<br/> 새로운 여행 플래너</h1>
-          <p class="content2">고민만 하던 여행 계획을<br/> 원트를 통해 한 번에 스케줄링 해보세요</p>
-          <v-btn v-if="isLogin" color="primary" @click="startMyRoad" to="/project/create">시작하기</v-btn>
-          <v-btn v-if="!isLogin" color="primary" @click="redirectToGoogle">시작하기</v-btn>
-        </v-col>
-        <v-col cols="12" md="6" class="map">
-          <img src="@/assets/test_back.png" class="backImage" alt="Map">
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app>
+  <v-container class="fill-height d-flex justify-center align-center">
+    <v-row>
+      <v-col cols="12" md="4" class="content">
+        <h1 style="font-weight: 700; margin-bottom: 20px">기존에 경험하지 못한<br/> 새로운 여행 플래너</h1>
+        <p>고민만 하던 여행 계획을<br/> 원트를 통해 한 번에 스케줄링 해보세요</p>
+        <v-btn v-if="isLogin" class="black-btn" @click="startMyRoad" to="/project/create">시작하기</v-btn>
+        <v-btn v-if="!isLogin" class="black-btn" @click="redirectToGoogle">시작하기</v-btn>
+      </v-col>
+      <v-col cols="12" md="8" class="map" style="right: 0">
+        <img src="@/assets/img/file.png" class="backImage" alt="Map">
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
 export default {
   name: 'TravelPlanner',
   data() {
@@ -63,3 +59,21 @@ export default {
   },
 };
 </script>
+
+<style>
+.content {
+  margin: auto 0;
+}
+
+.black-btn {
+  background-color: #000 !important; /* 검정색 배경 */
+  color: #fff !important; /* 흰색 글자 */
+  border: none !important; /* 기본 테두리 제거 */
+  font-weight: bold; /* 글자 두께 조절 */
+  text-transform: uppercase; /* 대문자 변환 */
+}
+
+.black-btn:hover {
+  background-color: #333 !important; /* 호버 시 배경색 변경 */
+}
+</style>
